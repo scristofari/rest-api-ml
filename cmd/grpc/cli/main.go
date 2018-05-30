a201ac2e61e9993047946a61b622da5a09b1c595
Sylvain Cristofari <s.cristofari@gmail.com>
2018-05-30 11:51:01 +0200

    add grpc server / cli

--- cmd/grpc/cli/main.go
+++ cmd/grpc/cli/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/scristofari/rest-api-ml/box"
+
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("grpc-cli")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("could not load the .env file")
+	}
+	port := os.Getenv("GRPC_PORT")
+
+	conn, err := grpc.Dial(":"+port, grpc.WithInsecure())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not connect to backend: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	client := box.NewBoxerClient(conn)
+	a, err := client.Run(context.Background(), &box.ArtifactRequest{})
+	if err != nil {
+		log.Fatalf("could not run : %v", err)
+	}
+	_ = a
 }
--- cmd/grpc/server/main.go
+++ cmd/grpc/server/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/scristofari/rest-api-ml/box"
+	"google.golang.org/grpc"
+)
+
+type BoxServer struct{}
+
+func (BoxServer) Run(context.Context, *box.ArtifactRequest) (*box.Artifact, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
 
 func main() {
-	fmt.Println("grpc-server")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("could not load the .env file")
+	}
+
+	s := grpc.NewServer()
+	box.RegisterBoxerServer(s, BoxServer{})
+
+	port := os.Getenv("GRPC_PORT")
+	l, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("could not listen to :%s: %v", port, err)
+	}
+	log.Fatal(s.Serve(l))
 }
